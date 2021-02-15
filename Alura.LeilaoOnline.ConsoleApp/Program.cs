1d1629e0420c64254199c1ddf5bfb456ff068889
Grace Tôrres Leite <grace.torres@dbserver.com.br>
2021-02-15 15:21:57 -0300

    update project

--- Alura.LeilaoOnline.ConsoleApp/Program.cs
+++ Alura.LeilaoOnline.ConsoleApp/Program.cs
@@ -5,18 +5,32 @@ namespace Alura.LeilaoOnline.ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void LeilaoComApenasUmLance()
         {
+            var leilaoPeca1 = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilaoPeca1);
 
+            leilaoPeca1.RecebeLance(fulano, 800.0);
+
+            //Act - método sob teste
+            leilaoPeca1.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 800;
+            var valorObtido = leilaoPeca1.Ganhador.Valor;
+
+            Verifica(valorEsperado, valorObtido);
+
+        }
+
+        private static void LeilaoComVariosLances()
+        {
             var leilaoPeca1 = new Leilao("Van Gogh");
 
             var fulano = new Interessada("Fulano", leilaoPeca1);
-            var maria = new Interessada("Maria",leilaoPeca1);
+            var maria = new Interessada("Maria", leilaoPeca1);
 
-            //Lance lanceFulano = new Lance(fulano,5000.0);
-            //Lance lanceMaria = new Lance(maria, 4000.0);
-
-            leilaoPeca1.RecebeLance(fulano,800.0);
+            leilaoPeca1.RecebeLance(fulano, 800.0);
             leilaoPeca1.RecebeLance(maria, 900.0);
             leilaoPeca1.RecebeLance(fulano, 1000.0);
             leilaoPeca1.RecebeLance(maria, 990.0);
@@ -28,15 +42,31 @@ namespace Alura.LeilaoOnline.ConsoleApp
             var valorEsperado = 1000;
             var valorObtido = leilaoPeca1.Ganhador.Valor;
 
-            if (valorEsperado == valorObtido)
+            Verifica(valorEsperado, valorObtido);
+
+        }
+
+        private static void Verifica(double esperado, double obtido)
+        {
+            var cor = Console.ForegroundColor;
+            if (esperado == obtido)
             {
-                Console.WriteLine("Test OK");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("TESTE OK");
             }
             else
             {
-                Console.WriteLine("Teste falhou");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(
+                    $"TESTE FALHOU! Esperado: {esperado}, obtido: {obtido}.");
             }
+            Console.ForegroundColor = cor;
+        }
 
+        static void Main(string[] args)
+        {
+            LeilaoComVariosLances();
+            LeilaoComApenasUmLance();
         }
     }
 }
