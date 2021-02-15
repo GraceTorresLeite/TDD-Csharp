7a6e4b6c03ff921bb382703dbd7abb0da6ec46f4
Grace Tôrres Leite <grace.torres@dbserver.com.br>
2021-02-15 16:05:25 -0300

    update project

--- Alura.LeilaoOnline/LeilaoTestes.cs
+++ Alura.LeilaoOnline/LeilaoTestes.cs
@@ -29,6 +29,36 @@ namespace Alura.LeilaoOnline
             Assert.Equal(valorEsperado, valorObtido);
 
         }
+
+        [Fact]
+        public void LeilaoComTresLances()
+        {
+            //Arrange
+            var leilaoPeca1 = new Leilao("Van Gogh");
+
+            var fulano = new Interessada("Fulano", leilaoPeca1);
+            var maria = new Interessada("Maria", leilaoPeca1);
+            var beltrano = new Interessada("Beltrano", leilaoPeca1);
+
+            leilaoPeca1.RecebeLance(fulano, 800.0);
+            leilaoPeca1.RecebeLance(maria, 900.0);
+            leilaoPeca1.RecebeLance(fulano, 1000.0);
+            leilaoPeca1.RecebeLance(maria, 990.0);
+            leilaoPeca1.RecebeLance(beltrano, 1400.0);
+
+            //Act - método sob teste
+            leilaoPeca1.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 1400;
+            var valorObtido = leilaoPeca1.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+            Assert.Equal(beltrano, leilaoPeca1.Ganhador.Cliente);
+
+        }
+
+
         [Fact]
         public void LeilaoComVariosLances()
         {
